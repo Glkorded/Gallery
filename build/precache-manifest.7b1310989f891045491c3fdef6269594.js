98da7cd84d4db751c33af5668254a8c234f21717
Gorbonosov Ilya <43453424+Glkorded@users.noreply.github.com>
2019-01-18 19:09:02 +0300

    Add files via upload

---
+++ build/precache-manifest.7b1310989f891045491c3fdef6269594.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fb50978571b60d2d4288f27bf80e7a3",
+    "url": "/static/media/11.1fb50978.jpg"
+  },
+  {
+    "revision": "6cd0c66674af3ab2149b",
+    "url": "/static/css/main.cbc242ed.chunk.css"
+  },
+  {
+    "revision": "21f61120e14c9923627d",
+    "url": "/static/js/1.21f61120.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d9af4456beff311752bd95987099186",
+    "url": "/static/media/0.9d9af445.jpg"
+  },
+  {
+    "revision": "a636d8da5e968e8abd5144c057af70a2",
+    "url": "/static/media/1.a636d8da.jpg"
+  },
+  {
+    "revision": "b0f26834973cd3f98721aa5bb6bc8b8a",
+    "url": "/static/media/10.b0f26834.jpg"
+  },
+  {
+    "revision": "6cd0c66674af3ab2149b",
+    "url": "/static/js/main.6cd0c666.chunk.js"
+  },
+  {
+    "revision": "21c76a87c9646659f0b50b75fb77c820",
+    "url": "/static/media/12.21c76a87.jpg"
+  },
+  {
+    "revision": "cdfff2e9f3547df49eec03b21d4a9b8e",
+    "url": "/static/media/2.cdfff2e9.jpg"
+  },
+  {
+    "revision": "4ccf6510dd572d2efd8e06a254e6d88d",
+    "url": "/static/media/3.4ccf6510.jpg"
+  },
+  {
+    "revision": "669d2382dcc73b2739b6b648369952f9",
+    "url": "/static/media/4.669d2382.jpg"
+  },
+  {
+    "revision": "cada063579782337d81c82dd6c268c84",
+    "url": "/static/media/7.cada0635.jpg"
+  },
+  {
+    "revision": "0f5769e50d7ee648c2ac964891ea318a",
+    "url": "/static/media/9.0f5769e5.jpg"
+  },
+  {
+    "revision": "99bd2fd5b1ecabf934e68056f8d2814f",
+    "url": "/index.html"
+  }
+];
